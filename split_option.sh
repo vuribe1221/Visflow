4494b399b2beb1fdbd390c257a388e27d2784cc2
vuribe <vuribe1221@gmail.com>
2013-11-25 21:42:24 -0800

    modified:   split_option.sh

--- split_option.sh
+++ split_option.sh
@@ -82,7 +82,7 @@ fi
 if [ $option == 3 ];then
 	read -p "How many times should it be split? " number_of_splits
 	# iget the file
-	if [ ! $? == 0];then	
+	if [ ! $? == 0 ];then	
 		sh login.sh
 	fi	
 
@@ -94,10 +94,12 @@ if [ $option == 3 ];then
                 rm -r chunks
         fi
         mkdir chunks
-
-	echo $file_to_split
+	
+	rm path	
+	echo $file_to_split >> path
+	tempfolder=$file_to_split
 	file_to_split=`basename $file_to_split`
-	#echo $file_to_split
+	echo $file_to_split
 
 	# split it
 	if [ -f $file_to_split ];then
@@ -105,6 +107,10 @@ if [ $option == 3 ];then
                 split -a 2 -dl$((`wc -l $file_to_split|sed 's/ .*$//'` / $number_of_splits + 1)) $file_to_split chunks/$file_to_split.
 
 	###### UPLOAD TO IRODS
+	tempfolder=`sed s/test1_grav_pos.txt//g path`
+	#echo $tempfolder
+	iput -r chunks/ $tempfolder
+	
 	
         else
         # if file is not found then break
