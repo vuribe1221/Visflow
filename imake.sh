5d45b8d871de0ef9fea8dc83b3e9cd428a3e2a04
vuribe <vuribe1221@gmail.com>
2013-11-29 17:16:42 -0800

    Moved helper scripts to bin also updated imake

--- imake.sh
+++ imake.sh
@@ -1,2 +1,43 @@
-#sh login.sh
-sh getFolder.sh $1
+##################### config ######################
+# use .sh for bash .py for python files
+script_suffix=.sh
+
+
+# login to get password and enable irods on local machine
+sh ~/imake/bins/login.sh
+
+# get irods working directory
+read -p "$(tput setaf 3)What is the irods working directory?$(tput sgr0) " cwd
+echo $cwd>>.config
+
+# if more than 1 script file exists prompt which to use
+dir=`head -n 2 .config|tail -n 1`
+num_of_scripts=`ils $dir|grep -c $script_suffix`
+#echo $num_of_scripts
+
+if [ $num_of_scripts -lt 1  ];then
+	echo "SCRIPT FILE NOT FOUND!"
+	exit 1
+else
+	if [ $num_of_scripts -gt 1 ];then
+		echo "$(tput setaf 2)MULTIPLE SCRIPTS FOUND!$(tput sgr0)"
+		ils $dir/|grep $script_suffix
+		read -p "$(tput setaf 3)Which file should be used?$(tput sgr0) " script_file
+	else
+		script_file=`ils $dir/*$script_suffix`
+	fi
+fi
+
+echo $script_file>>.config
+
+# get folder for data
+ils $dir/
+
+echo "Is the data in one file?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) read -p "$(tput setaf 3)What is the data file?$(tput sgr0) " data_file;echo "yes">>.config;echo $data_file >> .config;break;;
+        No ) read -p "$(tput setaf 3)What is the directory for the data files?$(tput sgr0)  " data_dir;echo "no">>.config;echo $data_dir >> .config;break;;
+    esac
+done
+
--- login.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#just in case the folder is downloaded but not exported
-export PATH=~/icommands/:{$PATH}
-
-#logs into irods to enable irods functionality
-read -s -p "Enter irods password: " pass
-iinit -e $pass
-echo
-
