5e0d77316b60a9f4c64815f0bdad53fa49f624d1
vuribe1221 <vuribe1221@gmail.com>
2013-12-08 01:00:21 -0600

    updated imake to allow for common files

--- imake.sh
+++ imake.sh
@@ -13,36 +13,49 @@ export PATH=~/icommands/:${PATH}
 # login to get password and enable irods on local machine
 sh ~/imake/bins/login.sh
 ils -r 
-
+if [ ! $? = 0 ];then
+	echo "An error occured when logging into irods! Bad password?"
+	exit 1
+fi
 # get irods working directory
 read -p "$(tput setaf 3)What is the irods working directory?$(tput sgr0) " cwd
 echo $cwd>>.config
-
+ils $cwd
 # if more than 1 script file exists prompt which to use
-dir=`head -n 2 .config|tail -n 1`
-num_of_scripts=`ils $dir|grep -c $script_suffix`
-
-if [ $num_of_scripts -lt 1  ];then
-	echo "SCRIPT FILE NOT FOUND!"
-	exit 1
-else
-	if [ $num_of_scripts -gt 1 ];then
-		echo "$(tput setaf 2)MULTIPLE SCRIPTS FOUND!$(tput sgr0)"
-		ils $dir/|grep $script_suffix
-		read -p "$(tput setaf 3)Which file should be used?$(tput sgr0) " script_file
-		read -p "$(tput setaf 3)Enter arguments needed for the script:$(tput sgr0) " arguments
+#dir=`head -n 2 .config|tail -n 1`
+if [ ! $script_suffix = NONE ];then
+	num_of_scripts=`ils $cwd|grep -c $script_suffix`
+	echo $num_of_scripts
+	if [ $num_of_scripts -lt 1  ];then
+		echo "SCRIPT FILE NOT FOUND!"
+		exit 1
 	else
-		script_file=`ils $dir/*$script_suffix`
-	
+		if [ $num_of_scripts -gt 1 ];then
+			echo "$(tput setaf 2)MULTIPLE SCRIPTS FOUND!$(tput sgr0)"
+			ils $cwd/|grep $script_suffix
+			read -p "$(tput setaf 3)Which file should be used?$(tput sgr0) " script_file
+		else
+			echo "Found script file!"
+			script_file=`ils $cwd/|grep $script_suffix`
+			echo "Using $script_file"
+		fi
 	fi
+else 
+	ils $cwd
+	read -p "$(tput setaf 3)What is the program file?$(tput sgr0) " script_file
 fi
-
-
+			read -p "$(tput setaf 3)Enter arguments needed for the script:$(tput sgr0) " arguments
+# check for a common file
+echo "Is there a common file?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) read -p "$(tput setaf 3)What is the common file?$(tput sgr0) " common_file;common=yes;break;;
+        No ) common=no;break;;
+    esac
+done
 
 # get folder for data
-ils $dir/
-
-echo "Is the data in one file?"
+echo "Is the data(input) in one file?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes ) read -p "$(tput setaf 3)What is the data file?$(tput sgr0) " data_file;yn=yes;break;;
@@ -51,7 +64,7 @@ select yn in "Yes" "No"; do
 done
 
 #get number of chunks
-num_chunks=`ils $dir/$data_dir|wc -l`
+num_chunks=`ils $cwd/$data_dir|wc -l`
 
 ## make the .config
 
@@ -61,6 +74,8 @@ num_chunks=`ils $dir/$data_dir|wc -l`
 	echo $data_file>>.config
 	echo $num_chunks>>.config
 	echo $arguments>>.config
+	echo $common>>.config
+	echo $common_file>>.config
 
 # make sure the .config file has the necessary info
 line=`wc -l .config|cut -d ' ' -f 1`
