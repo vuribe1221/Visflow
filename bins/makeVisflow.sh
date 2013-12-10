5ef90e5e3c695157d1561eb1139c86a2624b275d
vuribe1221 <vuribe1221@gmail.com>
2013-12-10 03:14:06 -0600

    modified:   bins/makeVisflow.sh

--- bins/makeVisflow.sh
+++ bins/makeVisflow.sh
@@ -19,12 +19,18 @@ com_sum=`head -n 10 .config|tail -n 1`
 #name=`basename $file_or_dir`
 name=`head -n 11 .config|tail -n 1`
 file_or_dir=`echo $file_or_dir|sed "s/\/$name//g"`
+usrname=`head -n 3 ~/.irods/.irodsEnv|tail -n 1|cut -d ' ' -f 2`
+echo $username
+echo "curl -s 'http://visflow.info/update.php?mode=reset&table=$usrname'">>this.sh;sh this.sh;rm this.sh;echo
+echo "curl -s 'http://visflow.info/update.php?mode=create&table=$usrname'">>this.sh;sh this.sh;rm this.sh;echo
+
 if [ $single_file = yes ];then
 	let num_chunks=1
 fi
 i=1
 while [ $i -lt $num_chunks ];
 do
+	echo "curl -s 'http://visflow.info/update.php?mode=add&table=$usrname&id=$i&status=1'">>./Makeflow_dir/$main$i
 	echo 'export PATH=~/icommands:${PATH}'>>./Makeflow_dir/$main$i
 	echo "
 	iinit -e $pass
@@ -80,5 +86,6 @@ else
 	fi
 fi
 
+	echo "curl -s 'http://visflow.info/update.php?mode=update&table=$usrname&id=$i&status=2'">>./Makeflow_dir/$main$i
 let i=$i+1
 done
